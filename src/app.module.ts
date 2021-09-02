93674eb95cf59aacc087b91625a9511327517ac4
Matheus Ferreira <math.ferr@outlook.com>
2021-09-02 18:54:06 -0300

    Config do banco de dados

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,17 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { ClienteModule } from './cliente/cliente.module';
 
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'sqlite',
+      storage: ':memory:',
+      autoLoadModels: true,
+    }),
+    ClienteModule,
+  ],
+  controllers: [],
+  providers: [],
 })
 export class AppModule {}
