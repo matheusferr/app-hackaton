8cca83e8207a0634af520db7fc69cdd86cc49617
Matheus <math.ferr@outlook.com>
2021-09-06 16:14:51 -0300

    Configuração do .env

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,12 +1,17 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
 import { SequelizeModule } from '@nestjs/sequelize';
 import { ClienteModule } from './cliente/cliente.module';
 
 @Module({
   imports: [
+    ConfigModule.forRoot(),
     SequelizeModule.forRoot({
-      dialect: 'sqlite',
-      storage: ':memory:',
+      dialect: 'postgres',
+      host: process.env.DB_HOST,
+      username: process.env.DB_USER,
+      password: process.env.DB_PASS,
+      database: process.env.DB_NAME,
       autoLoadModels: true,
     }),
     ClienteModule,
